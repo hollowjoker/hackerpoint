c50f932bb308252e18fc7961cfa9f49b12fdb6c0
Sam Esquejo <sam.esquejo@august99.com>
2018-11-17 10:07:51 +0800

    fix routes

--- app/Modules/Admin/Dashboard/routes.php
+++ app/Modules/Admin/Dashboard/routes.php
@@ -2,7 +2,7 @@
 	
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => '/dashboard', 'namespace' => '\App\Modules\Admin\Dashboard\Controllers', 'middleware' => ['web']], function(){
+Route::group(['prefix' => '/admin/dashboard', 'namespace' => '\App\Modules\Admin\Dashboard\Controllers', 'middleware' => ['web']], function(){
 	Route::get('/', 'DashboardController@index')->name('dashboard.index');
 });
 
