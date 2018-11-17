cb241f8cca65ee461a5361a620b3a078720fe4e7
Lordwill Mabalot <lordwill.mabalot@august99.com>
2018-11-17 13:57:52 +0800

    Updated front main

--- app/Modules/Front/Home/Views/index.blade.php
+++ app/Modules/Front/Home/Views/index.blade.php
@@ -1,14 +1,15 @@
 @extends('layout.front.master')
 @section('content')
-  <div class="main">
-    <section id="home">
+  <main class="main">
+    <section id="home" class="section section-home">
       <div class="container">
-        
+        <h1>Lorem Ipsum</h1>
+        <h4>Sed ut ultrices purus, sed gravida quam. Vestibulum ornare sed metus eget pharetra. Integer sagittis ligula scelerisque, luctus arcu eget, consectetur turpis.</h4>
       </div>
     </section>
-    <section id="about">about</section>
-    <section id="shop">shop</section>
-    <section id="reviews">reviews</section>
-    <section id="contact">contact</section>
-  </div>
+    <section id="about" class="section section-about">about</section>
+    <section id="shop" class="section section-shop">shop</section>
+    <section id="reviews" class="section section-reviews">reviews</section>
+    <section id="contact" class="section section-contact">contact</section>
+  </main>
 @stop
