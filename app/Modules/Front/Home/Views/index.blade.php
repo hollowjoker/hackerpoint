2c24f9369cea94666533a3b933c0d3ed13acd8a5
Lordwill Mabalot <lordwill.mabalot@august99.com>
2018-11-17 13:14:09 +0800

    Updated header

--- app/Modules/Front/Home/Views/index.blade.php
+++ app/Modules/Front/Home/Views/index.blade.php
@@ -1,6 +1,14 @@
 @extends('layout.front.master')
 @section('content')
   <div class="main">
-    
+    <section id="home">
+      <div class="container">
+        
+      </div>
+    </section>
+    <section id="about">about</section>
+    <section id="shop">shop</section>
+    <section id="reviews">reviews</section>
+    <section id="contact">contact</section>
   </div>
 @stop
--- resources/views/layout/front/header.blade.php
+++ resources/views/layout/front/header.blade.php
@@ -1,22 +1,36 @@
 <header class="header">
   <div class="container">
-    <div class="row align-items-center">
-      <div class="col-6">
+    <div class="row align-items-center justify-content-between">
+      <div class="col-4">
         <div class="header__logo-wrapper">
-          <h4><span class="txt-strong">HACKDOG</span><span class="txt-light">Manila</span></h4>
+          <h4><a href=""><span class="txt-strong">HACKDOG</span><span class="txt-light">Manila</span></a></h4>
         </div>
       </div>
-      <div class="col-6">
+      <div class="col-4">
         <nav class="header__nav-wrapper">
           <ul class="d-flex align-items-center justify-content-end">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Shop</a></li>
-            <li><a href="#">Reviews</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Login</a></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#shop">Shop</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#reviews">Reviews</a></li>
+            <li><a href="#contact">Contact</a></li>
           </ul>
         </nav>
+      </div>
+      <div class="col-4">
+        <div class="header__controls-wrapper d-flex align-items-center justify-content-end">
+          <div class="header-control">
+            <button class="btn--custom btn-search"><i class="material-icons">search</i></button>
+          </div>
+          <div class="header-control">
+            <a class="btn--custom btn-login"><i class="material-icons">person</i></a>
+          </div>
+          <div class="header-control">
+            <button class="btn--custom btn-cart">
+              <i class="material-icons">shopping_cart</i>
+              <div class="btn-cart--count">2</div>
+            </button></div>
+        </div>
       </div>
     </div>
   </div>
